436553347269dfcd928eb350fdaf273ad63b1c57
hasan <mohammad.hasan.najafii.2004@gmail.com>
2024-12-27 20:00:06 +0330

    feat: made a command to create bulk of access code

---
+++ backend/users/management/commands/create_access_code.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand, CommandError
+from users.models import AccessCode as Code
+from django.utils import timezone
+
+
+
+class Command(BaseCommand):
+    help = "Creates single Access Code and sends to a choosen destination (optional)"
+
+    def add_arguments(self, parser):
+        parser.add_argument("numbers", type=int)
+
+    def handle(self, *args, **options):
+        numbers = options["numbers"]
+        for number in range(numbers):
+            try:
+                access_code = Code.objects.create(date_created=timezone.localtime())
+            except Exception as e:
+                raise CommandError(f"Opration Failed. \nError: {e}")
+
+            self.stdout.write(
+                self.style.SUCCESS(f"Successfully Created {access_code.code}")
+            )
+
